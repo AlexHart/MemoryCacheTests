3da53fd9f5e41826ab2530116fbde1c532d8c361
Alejandro Hartlöhner <hartformer@hotmail.com>
2018-07-21 13:42:42 +0200

    Add MemoryCache implementation

--- MemoryCacheTests/Program.fs
+++ MemoryCacheTests/Program.fs
@@ -1,7 +1,41 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿open System
+open System.Runtime.Caching
+open System.Threading
+
+let keyName = "cached_date"
+
+let cacheSecondsDuration = 3.0
+
+let getDate() = DateTime.Now
+
+// Refresh item inside the cache.
+let setCache (cache: MemoryCache) = 
+    // Create expire policy.
+    let policy = new CacheItemPolicy()
+    policy.AbsoluteExpiration <- DateTimeOffset.Now + TimeSpan.FromSeconds(cacheSecondsDuration)
+
+    // Set item in cache.
+    cache.Set(keyName, getDate(),  policy) |> ignore
 
 [<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+let main argv =    
+    let cache = MemoryCache.Default
+
+    printfn "Cache that expires every %f seconds" cacheSecondsDuration
+
+    [1..15]
+    |> List.iter(fun iteration ->
+        Thread.Sleep 1000
+        
+        // Check that the item is not in the cache and add it.
+        if (cache.[keyName] = null) then
+            setCache cache
+        
+        // Get the item from the cache and print it.
+        let cacheContents = cache.[keyName]
+        let date: DateTime = Convert.ToDateTime(cacheContents)
+        let dateAsString = date.ToLongTimeString() + " - " + date.Ticks.ToString()
+
+        printfn "%d\t%s\tcached: %s" iteration (DateTime.Now.ToLongTimeString()) dateAsString)
+        
+    0

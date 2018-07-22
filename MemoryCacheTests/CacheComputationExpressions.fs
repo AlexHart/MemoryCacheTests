2e28c24698da848544a85283f67e0af2dff5d36b
Alejandro Hartlöhner <hartformer@hotmail.com>
2018-07-22 11:46:42 +0200

    Add simple computation expressions for the cache handling. Just for fun.

---
+++ MemoryCacheTests/CacheComputationExpressions.fs
@@ -0,0 +1,47 @@
+﻿module CacheComputationExpressions
+
+    open System
+    open System.Runtime.Caching
+
+    // Refresh item inside the cache.
+    let setCache (cache: MemoryCache) key value cacheSecondsDuration = 
+        // Create expire policy.
+        let policy = new CacheItemPolicy()
+        policy.AbsoluteExpiration <- DateTimeOffset.Now + TimeSpan.FromSeconds(cacheSecondsDuration)
+
+        // Set item in cache.
+        cache.Set(key, value,  policy)
+
+    /// Computation expression for accessing cache with variable key names.
+    type MemoryCacheExpression(cache: MemoryCache, cacheSecondsDuration) =
+        member x.Bind((key, valueFun: unit -> 'b), func: 'b -> 'b) =
+            // If it's not in the cache, add it.
+            if cache.[key] = null then 
+                setCache cache key (valueFun()) cacheSecondsDuration
+
+            // It will return the right type iso of obj.
+            func (cache.[key] :?> 'b)
+        
+        /// Return just an item of the current type.
+        member x.Return(value) = value
+
+        /// Return from the cache by passing the key.
+        member x.ReturnFrom(keyName) = 
+            cache.[keyName] :?> 'b
+
+    /// Computation expression for accessing cache always with the same key name.
+    type SingleNameMemoryCacheExpression(cache: MemoryCache, cacheSecondsDuration) =
+        let fixedKey = Guid.NewGuid().ToString()
+
+        member x.Bind(valueFun: unit -> 'b, func: 'b -> 'b) =
+            // It it's not in the cache, add it.
+            if cache.[fixedKey] = null then
+                setCache cache fixedKey (valueFun()) cacheSecondsDuration
+
+            func (cache.[fixedKey] :?> 'b)
+
+        /// Return just an item of the current type.
+        member x.Return(value) = value
+
+        /// Return from the cache by passing the key.
+        member x.ReturnFrom(keyName) = cache.[keyName] :?> 'b
--- MemoryCacheTests/Program.fs
+++ MemoryCacheTests/Program.fs
@@ -1,41 +1,43 @@
 ﻿open System
 open System.Runtime.Caching
 open System.Threading
+open CacheComputationExpressions
 
 let keyName = "cached_date"
-
 let cacheSecondsDuration = 3.0
-
 let getDate() = DateTime.Now
-
-// Refresh item inside the cache.
-let setCache (cache: MemoryCache) = 
-    // Create expire policy.
-    let policy = new CacheItemPolicy()
-    policy.AbsoluteExpiration <- DateTimeOffset.Now + TimeSpan.FromSeconds(cacheSecondsDuration)
-
-    // Set item in cache.
-    cache.Set(keyName, getDate(),  policy) |> ignore
 
 [<EntryPoint>]
 let main argv =    
+    // Create cache.
     let cache = MemoryCache.Default
 
     printfn "Cache that expires every %f seconds" cacheSecondsDuration
 
+    // Build the expression.
+    let dateCache = new MemoryCacheExpression(cache, cacheSecondsDuration)
+
     [1..15]
     |> List.iter(fun iteration ->
+        // Loop every second.
         Thread.Sleep 1000
-        
-        // Check that the item is not in the cache and add it.
-        if (cache.[keyName] = null) then
-            setCache cache
-        
-        // Get the item from the cache and print it.
-        let cacheContents = cache.[keyName]
-        let date: DateTime = Convert.ToDateTime(cacheContents)
-        let dateAsString = date.ToLongTimeString() + " - " + date.Ticks.ToString()
 
-        printfn "%d\t%s\tcached: %s" iteration (DateTime.Now.ToLongTimeString()) dateAsString)
-        
+        let date = dateCache {
+            // It will only execute getDate if the value is not in the cache.
+            let! dateFromCache = (keyName, getDate)
+
+            // Print the cached value.
+            let dateAsString = dateFromCache.ToLongTimeString() + " - " + dateFromCache.Ticks.ToString()
+            printfn "%d\tNow: %s\tcached: %s" iteration (DateTime.Now.ToLongTimeString()) dateAsString
+
+            // Using the key you could return the item in the cache.
+            // But it could just be updated, so it depends on the users preference.
+            // return! keyName 
+
+            // Without the ! just return the cached value.
+            return dateFromCache
+        }
+
+        printfn "Returned from computation expression: %s" (date.ToLongTimeString()))
+    
     0
